8f0e26f371f551ca9aff7c20adf5a6e2ffac548c
Glenn <phoobynet@users.noreply.github.com>
2023-01-24 14:35:00 +0000

    wip

--- src/routes/card-entry/CardEntry.tsx
+++ src/routes/card-entry/CardEntry.tsx
@@ -18,79 +18,90 @@ export default function CardEntry () {
   const [success, setSuccess] = useState<boolean>(false)
   const [cardFormData, setCardFormData] = useState<CardFormData>({ ...CardFormDataDefault })
   return (
-    <div className={styles.cardEntry}>
-      <div className={styles.cardBack}>
-        <CardBack cvc={cardFormData.cvc} />
-      </div>
-      <div className={styles.cardFront}>
-        <CardFront
-          cardNumber={cardFormData.cardNumber}
-          cardExpiryMonth={cardFormData.expiryMonth}
-          cardExpiryYear={cardFormData.expiryYear}
-          cardName={cardFormData.cardholderName}
-        />
-      </div>
-      <main>
-        <AnimatePresence>
-          {success && <motion.div
-            key="0"
-            initial={{ x: 300, opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            exit={{ x: -300, opacity: 0 }}
-          ><ThankYou /></motion.div>}
-          {!success && <motion.div key="1"><Formik
-            initialValues={{ ...CardFormDataDefault }}
-            onSubmit={(values, actions) => {
-              console.log('Submit called')
-              setSuccess(true)
-              setCardFormData({
-                ...values,
-              })
-            }}
-            validate={cardFormValidation}
-          >{props => {
-            return (
-              <Form
-                className={styles.cardForm}
-              >
-                <CardholderNameGroup />
-                <CardNumberGroup />
+    <div className={styles.container}>
+      <div className={styles.cardEntry}>
+        <div className={styles.cardBack}>
+          <CardBack cvc={cardFormData.cvc} />
+        </div>
+        <div className={styles.cardFront}>
+          <CardFront
+            cardNumber={cardFormData.cardNumber}
+            cardExpiryMonth={cardFormData.expiryMonth}
+            cardExpiryYear={cardFormData.expiryYear}
+            cardName={cardFormData.cardholderName}
+          />
+        </div>
+        <main>
+          <AnimatePresence>
+            {success && <motion.div
+              key="0"
+              initial={{
+                x: 300,
+                opacity: 0,
+              }}
+              animate={{
+                x: 0,
+                opacity: 1,
+              }}
+              exit={{
+                x: -300,
+                opacity: 0,
+              }}
+            ><ThankYou /></motion.div>}
+            {!success && <motion.div key="1"><Formik
+              initialValues={{ ...CardFormDataDefault }}
+              onSubmit={(values, actions) => {
+                console.log('Submit called')
+                setSuccess(true)
+                setCardFormData({
+                  ...values,
+                })
+              }}
+              validate={cardFormValidation}
+            >{props => {
+              return (
+                <Form
+                  className={styles.cardForm}
+                >
+                  <CardholderNameGroup />
+                  <CardNumberGroup />
 
-                <div className={styles.expiryCvcGroup}>
-                  <label
-                    htmlFor="expiryMonth"
-                    className={styles.expiryLabel}
-                  >Exp. date (mm/yy)</label>
-                  <div className={styles.expiryMonthInput}>
-                    <CardExpiryMonthInput />
+                  <div className={styles.expiryCvcGroup}>
+                    <label
+                      htmlFor="expiryMonth"
+                      className={styles.expiryLabel}
+                    >Exp. date (mm/yy)</label>
+                    <div className={styles.expiryMonthInput}>
+                      <CardExpiryMonthInput />
+                    </div>
+                    <div className={styles.expiryYearInput}>
+                      <CardExpiryYearInput />
+                    </div>
+                    {(props.touched.expiryMonth || props.touched.expiryYear) && (props.errors.expiryMonth || props.errors.expiryYear)
+                      ? <span
+                        className={`error ${styles.expiryError}`}
+                      >{props.errors.expiryMonth || props.errors.expiryYear}</span>
+                      : null}
+                    <label
+                      htmlFor="cvc"
+                      className={styles.cvcLabel}
+                    >CVC</label>
+                    <div className={styles.cvcInput}>
+                      <CardCvcInput />
+                    </div>
+                    {props.touched.cvc && props.errors.cvc
+                      ? <span
+                        className={`error ${styles.cvcError}`}
+                      >{props.errors.cvc}</span>
+                      : null}
                   </div>
-                  <div className={styles.expiryYearInput}>
-                    <CardExpiryYearInput />
-                  </div>
-                  {(props.touched.expiryMonth || props.touched.expiryYear) && (props.errors.expiryMonth || props.errors.expiryYear)
-                    ? <span
-                      className={`error ${styles.expiryError}`}
-                    >{props.errors.expiryMonth || props.errors.expiryYear}</span>
-                    : null}
-                  <label
-                    htmlFor="cvc"
-                    className={styles.cvcLabel}
-                  >CVC</label>
-                  <div className={styles.cvcInput}>
-                    <CardCvcInput />
-                  </div>
-                  {props.touched.cvc && props.errors.cvc
-                    ? <span
-                      className={`error ${styles.cvcError}`}
-                    >{props.errors.cvc}</span>
-                    : null}
-                </div>
-                <Button type="submit">Confirm</Button>
-              </Form>)
-          }}
-          </Formik></motion.div>}
-        </AnimatePresence>
-      </main>
+                  <Button type="submit">Confirm</Button>
+                </Form>)
+            }}
+            </Formik></motion.div>}
+          </AnimatePresence>
+        </main>
+      </div>
     </div>
   )
 }
